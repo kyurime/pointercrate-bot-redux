c6e873b501af5186674b090d597c9ce057220876
zmxhawrhbg <25387744+kyurime@users.noreply.github.com>
2021-01-31 12:08:13 -0700

    implement demon adding

--- src/commands/demon/add.ts
+++ src/commands/demon/add.ts
@@ -1,4 +1,5 @@
 import { ApplicationCommandOptionType, Interaction, InteractionResponseType, MessageFlags } from "slash-commands";
+import { get_user, Permissions } from "../../database/user";
 import { shared_client } from "../../pointercrate-link";
 import Subcommand from "../../utils/subcommand";
 
@@ -65,11 +66,44 @@ export default class AddDemonCommand extends Subcommand {
 	) {
 		const client = shared_client();
 
+		const user = await get_user(interaction.member.user.id);
+		if (!user) {
+			return {
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					flags: MessageFlags.EPHEMERAL,
+					content: "You must be linked to use this command!",
+				}
+			}
+		}
+
+		if (!user.implied_permissions.includes(Permissions.ListModerator)) {
+			return {
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					flags: MessageFlags.EPHEMERAL,
+					content: "You must be list moderator to use this command!",
+				}
+			}
+		}
+
+		client.token_login_unsafe(user.token);
+
+		const creators_list = creators.split(",").map((creator_name) => creator_name.trim())
+
+		const demon = await client.demons.add({
+			name, position, requirement, verifier, publisher,
+			creators: creators_list,
+			video
+		});
+
+		client.logout();
+
 		return {
 			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 			data: {
-				content: "This command is currently unimplemented!",
 				flags: MessageFlags.EPHEMERAL,
+				content: `Demon ${demon.name} (ID: ${demon.id}) has been successfully added!`,
 			}
 		}
 	};
